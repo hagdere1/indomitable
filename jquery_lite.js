16ec2acea7a5ea72e6b3ab619cdcda60458dc139
Haik Gregory Agdere <hagdere1@binghamton.edu>
2016-03-11 09:35:44 -0500

    First commit.

---
+++ jquery_lite.js
@@ -0,0 +1,49 @@
+(function (root) {
+  var DOMNodeCollection = function () {
+
+  };
+
+  DOMNodeCollection.prototype.html = function () {
+
+  };
+
+
+  DOMNodeCollection.prototype.empty = function () {
+
+  };
+
+  DOMNodeCollection.prototype.remove = function () {
+
+  };
+
+  DOMNodeCollection.prototype.append = function () {
+
+  };
+
+  DOMNodeCollection.prototype.attr = function () {
+
+  };
+
+  DOMNodeCollection.prototype.addClass = function () {
+
+  };
+
+  DOMNodeCollection.prototype.removeClass = function () {
+
+  };
+
+
+  DOMNodeCollection.prototype.find = function () {
+
+  };
+
+  DOMNodeCollection.prototype.children = function () {
+
+  };
+
+  DOMNodeCollection.prototype.parent = function () {
+
+  };
+
+
+})(this);
